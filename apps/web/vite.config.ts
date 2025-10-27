2d5613570602b25bcd47a00fb32048e85a044fd7
JesusP2 <jesus_perez99@hotmail.com>
2025-10-27 01:54:46 -0600

    vite pwa doesnt work rn

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,39 +3,35 @@ import tailwindcss from '@tailwindcss/vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import { VitePWA } from "vite-plugin-pwa";
 import tsconfigPaths from 'vite-tsconfig-paths';
+// import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     cloudflare({ viteEnvironment: { name: 'ssr' } }),
     tsconfigPaths(),
     tailwindcss(),
-    tanstackStart({
-      spa: {
-        enabled: true,
-      },
-    }),
+    tanstackStart(),
     viteReact(),
-    VitePWA({
-      registerType: "autoUpdate",
-      devOptions: {
-        enabled: true,
-      },
-      workbox: {
-        clientsClaim: true,
-        skipWaiting: true,
-      },
-      manifest: {
-        name: "template",
-        short_name: "template",
-        description: "template - PWA Application",
-        theme_color: "#0c0c0c",
-      },
-      pwaAssets: {
-        disabled: false,
-        config: true,
-      },
-    }),
+    // VitePWA({
+    //   registerType: "autoUpdate",
+    //   devOptions: {
+    //     enabled: true,
+    //   },
+    //   workbox: {
+    //     clientsClaim: true,
+    //     skipWaiting: true,
+    //   },
+    //   manifest: {
+    //     name: "template",
+    //     short_name: "template",
+    //     description: "template - PWA Application",
+    //     theme_color: "#0c0c0c",
+    //   },
+    //   pwaAssets: {
+    //     disabled: false,
+    //     config: true,
+    //   },
+    // }),
   ],
 });
