ddc1033ba0dcf54d2805e5c59ef8be5d787a4fb4
JesusP2 <jesus_perez99@hotmail.com>
2025-11-08 23:54:46 -0600

    added redis env

--- apps/web/src/shared/env.ts
+++ apps/web/src/shared/env.ts
@@ -1,9 +1,10 @@
-import { z } from 'zod';
+import { z } from "zod";
 export const env = z
   .object({
     DATABASE_URL: z.string(),
     VITE_SERVER_URL: z.string(),
     BETTER_AUTH_SECRET: z.string(),
     TURNSTILE_SECRET: z.string(),
+    REDIS_URL: z.string(),
   })
   .parse(process.env);
